94ce572aa55156b3857d046a4ecaa54f11daff92
pratiksharm <sharma.pratik2016@gmail.com>
2021-03-19 16:11:34 +0530

    added a homePage

---
+++ src/homepage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div className="container">
+            <h1 className="text-center">
+                Learn Code in Hindi
+            </h1>
+        </div>
+    )
+}
+
+export default HomePage;
+
