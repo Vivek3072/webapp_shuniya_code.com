4333852f791b745a2fb552a93adab942e2646c12
Vivek Srivastava <vivek@shuniyavigyan.com>
2023-05-06 01:54:27 +0530

    Editor styling
    Co-Authored-By: Vivek Srivastava <88632897+Vivek3072@users.noreply.github.com>

--- src/CodeEditor/CodeEditor.js
+++ src/CodeEditor/CodeEditor.js
@@ -1,8 +1,4 @@
-import React,{useState,useRef, useEffect} from "react";
-// import { ReactTransliterate } from "./Translator/index";
-// import Highlight, { defaultProps } from "prism-react-renderer";
-// import Dark from "prism-react-renderer/themes/nightOwl";
-// import Light from "prism-react-renderer/themes/duotoneLight";
+import React, { useState, useRef } from "react";
 
 import {
   File,
@@ -11,7 +7,6 @@ import {
   // MoonFill,
 } from "react-bootstrap-icons";
 import "./CodeEditor.css";
-// import { languages } from './languages';
 
 const CodeEditor = ({
   texteditor,
@@ -29,103 +24,75 @@ const CodeEditor = ({
     const lines = event.target.value.split("\n").length;
     setLines(lines);
     handleChange(event);
-
-    // if (lines > lastLine) {
-    //   setlastLine(lines);
-    // }
-    
   };
 
   const lineNumbers = [];
   for (let i = 1; i <= lines; i++) {
-    const style = i === currentLine ? {fontWeight: "bold"} : {};
-    lineNumbers.push(<div key={i} style={style} >{i}</div>);
+    const style = i === currentLine ? { fontWeight: "bold" } : {};
+    lineNumbers.push(
+      <div key={i} style={style}>
+        {i}
+      </div>
+    );
   }
   const firstDivRef = useRef();
   const secondDivRef = useRef();
- 
-  const handleScrollFirst = scroll => {
+
+  const handleScrollFirst = (scroll) => {
     secondDivRef.current.scrollTop = scroll.target.scrollTop;
   };
-  
-  const handleScrollSecond = scroll => {
+
+  const handleScrollSecond = (scroll) => {
     firstDivRef.current.scrollTop = scroll.target.scrollTop;
-  }
-//input tab key wala 
+  };
+  //input tab key wala
   const inputRef = useRef(null);
 
-  function handleKeyDown(event){
-    if(event.key === 'Tab') {
-      event.preventDefault();//prevent default behaviour
+  function handleKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
       const { current } = inputRef;
-      const {selectionStart, selectionEnd } = current;
+      const { selectionStart, selectionEnd } = current;
       const value = current.value;
-      current.value = value.substring(0, selectionStart) + '     ' + value.substring(selectionEnd);
-      current.selectionStart=current.selectionEnd=selectionStart + 4;
+      current.value =
+        value.substring(0, selectionStart) +
+        "     " +
+        value.substring(selectionEnd);
+      current.selectionStart = current.selectionEnd = selectionStart + 4;
     }
   }
 
-
-  // const [lang, setLang] = useState("hi");
-  // const [dark, setDark] = useState(true);
-  // const [disable, setDisable] = useState(true);
-
-  // const theme = dark ? Dark : Light
-
-  // const styles = {
-  //   root: {
-  //     boxSizing: 'border-box',
-  //     fontFamily: '"Dank Mono", "Fira Code", monospace',
-  //     minHeight: '200px',
-  //     backgroundColor:'#000000',
-  //     ...theme.plain
-  //   }
-  // }
-
-  // const [show, setShow] = useState(false)
-
-  // const componentRef = useRef();
-
-  // const highlight = code => (
-  //   <Highlight {...defaultProps} theme={dark? Dark : Light} code={code} language="py">
-  //     {({ className, style, tokens, getLineProps, getTokenProps }) => (
-  //       <Fragment>
-  //         {tokens.map((line, i) => (
-  //           <div {...getLineProps({ line, key: i })}>
-  //             {line.map((token, key) => <span {...getTokenProps({ token, key })} />)}
-  //           </div>
-  //         ))}
-  //       </Fragment>
-  //     )}
-  //   </Highlight>
-  // )  
-        const textArea = document.querySelector("#code")
-        if ( textArea ){
-          const linenumber = textArea.value.substr(0, textArea.selectionStart).split("\n").length;
-          console.log(linenumber)
-          if ( linenumber !== currentLine )
-          setCurrentLine(linenumber)
-        }
+  const textArea = document.querySelector("#code");
+  if (textArea) {
+    const linenumber = textArea.value
+      .substr(0, textArea.selectionStart)
+      .split("\n").length;
+    console.log(linenumber);
+    if (linenumber !== currentLine) setCurrentLine(linenumber);
+  }
 
   return (
     <div className="code-editor-container row ">
-      <div className="line-numbers col-1 mt-1" onScroll={handleScrollFirst} ref={firstDivRef}>{lineNumbers}</div>
-      <textarea
-      id="code"
-      onScroll={handleScrollSecond} //ref={secondDivRef}
-      ref={inputRef} type="text" onKeyDown={handleKeyDown}
-        rows={lines}
-        value={texteditor}
-        onChange={handleLineChange}
-        className="editor_class mt-1 multi-input p-1 col-10"
-      ></textarea>
-
-
-      {/* <div>
-        <span>First item</span>
-        {"\t"}
-        <span>Second item</span>
-      </div> */}
+      <div className="editor_main mx-2">
+        <div
+          className="line-numbers mt-1"
+          onScroll={handleScrollFirst}
+          ref={firstDivRef}
+        >
+          {lineNumbers}
+        </div>
+        <textarea
+          id="code"
+          onScroll={handleScrollSecond} //ref={secondDivRef}
+          ref={inputRef}
+          type="text"
+          onKeyDown={handleKeyDown}
+          rows={lines}
+          value={texteditor}
+          onChange={handleLineChange}
+          className="editor_class mt-1 multi-input p-1 col-10"
+        ></textarea>
+      </div>
 
       {/* <ReactTransliterate
         language="python"
@@ -145,7 +112,7 @@ const CodeEditor = ({
         <div>यूजर इनपुट </div>
         <div onClick={setcustomInput}>
           {customInput ? (
-            <FileCheck color="green" size={20}/>
+            <FileCheck color="green" size={20} />
           ) : (
             <File size={20} />
           )}
