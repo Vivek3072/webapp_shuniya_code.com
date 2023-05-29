77e2d1a7ba22c1a696af702010146406e9ecfe3b
Sahil pillania <sahilpillania88@gmail.com>
2023-05-29 11:22:29 +0530

    Added language toggling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 function Footer() {
+  // getting language state
+  const language = useSelector((state) => state.language); // for getting the lang.
   return (
     <footer className="text-center text-lg-start bg-dark text-light">
       <section className="">
@@ -13,10 +16,9 @@ function Footer() {
               </div>
               <div className="fs-6 text-left">by Shuniyavigyan</div>
               <div className="text-left text-secondary my-1">
-                We are an online platform who aim to teach students coding in
-                their native language.This is to help all those students out
-                there who want to learn coding but can't because of language
-                barrier!
+                {language === "ENG"
+                  ? "We are an online platform who aim to teach students coding in their nativelanguage.This is to help all those students out there who want to learn coding but can't because of language barrier!"
+                  : "हम एक ऑनलाइन प्लेटफ़ॉर्म हैं, जिसका उद्देश्य छात्रों को उनकी मूल भाषा में कोडिंग सिखाना है। यह उन सभी छात्रों की मदद करने के लिए है जो कोडिंग सीखना चाहते हैं, लेकिन भाषा की बाधा के कारण नहीं सीख पाते हैं!"}
               </div>
             </div>
 
@@ -24,19 +26,33 @@ function Footer() {
               <h6 className="text-uppercase fw-bold mb-2">Legals</h6>
               <ul className="text-left">
                 <Link to="/contact">
-                  <li className="p-1">Contact Us</li>
+                  <li className="p-1">
+                    {language === "ENG" ? "Contact Us" : "संपर्क करें"}
+                  </li>
                 </Link>
                 <Link to="/about">
-                  <li className="p-1">About Us</li>
+                  <li className="p-1">
+                    {language === "ENG" ? "About Us" : "संबंध"}
+                  </li>
                 </Link>
                 <Link to="/privacy-policy">
-                  <li className="p-1">Privacy Policy</li>
+                  <li className="p-1">
+                    {language === "ENG" ? "Privacy Policy" : "गोपनीयता नीति"}
+                  </li>
                 </Link>
                 <Link to="/refund-policy">
-                  <li className="p-1">Refund Policy</li>
+                  <li className="p-1">
+                    {language === "ENG"
+                      ? "Refund Policy"
+                      : "भुगतान वापसी की नीति"}
+                  </li>
                 </Link>
                 <Link to="/terms-conditions">
-                  <li className="p-1">Terms & Conditions</li>
+                  <li className="p-1">
+                    {language === "ENG"
+                      ? "Terms & Conditions"
+                      : "नियम एवं शर्तें"}
+                  </li>
                 </Link>
               </ul>
             </div>
