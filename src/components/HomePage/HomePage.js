6e1129224bd89fbfa9122279b00c25881cf0bd9f
Sahil pillania <sahilpillania88@gmail.com>
2023-05-29 13:34:42 +0530

    Added Hindi text toggling

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,8 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Coding from "../../../src/assets/Coding.svg";
 import Services from "./Services";
+import { useSelector } from "react-redux";
 
 export default function HomePage() {
+  // getting language state
+  const language = useSelector((state) => state.language); // for getting the lang.
   return (
     <>
       <section id="hero" className="d-flex align-items-center">
@@ -11,11 +14,14 @@ export default function HomePage() {
           <div className="row">
             <div className="col-lg-6 pt-5 pt-lg-0 order-1 order-lg-1 d-flex flex-column justify-content-center">
               <h1 className="m-0 fs-1">
-                Learn coding from the best without any barrier!
+                {language === "ENG"
+                  ? "Learn coding from the best without any barrier!"
+                  : "सर्वश्रेष्ठ से बिना किसी बाधा के कोडिंग सीखें!"}
               </h1>
               <div className="my-3">
-                We are here with a solution for those students who always wanted
-                to learn coding but can't because of language barrier
+                {language === "ENG"
+                  ? "We are here with a solution for those students who always wanted to learn coding but can't because of language barrier"
+                  : "हम यहां उन छात्रों के लिए एक समाधान लेकर आए हैं जो हमेशा कोडिंग सीखना चाहते थे लेकिन भाषा की बाधा के कारण नहीं सीख पाते।"}
               </div>
               <div className="d-flex flex-row">
                 <Link to="/editor" className="mx-2">
@@ -23,7 +29,9 @@ export default function HomePage() {
                     className="bg-primary rounded rounded-5 px-3 py-2 text-light text-center"
                     style={{ width: "fit-content" }}
                   >
-                    Start Coding
+                    {language === "ENG"
+                      ? " Start Coding"
+                      : "कोडिंग प्रारंभ करें"}
                   </div>
                 </Link>
 
@@ -32,7 +40,7 @@ export default function HomePage() {
                     className="border border-secondary rounded rounded-5 px-3 py-2 text-secondary text-center"
                     style={{ width: "fit-content" }}
                   >
-                    View Courses
+                    {language === "ENG" ? "View Courses" : "पाठ्यक्रम देखें"}
                   </div>
                 </Link>
               </div>
@@ -48,14 +56,21 @@ export default function HomePage() {
       <div className="container mt-5">
         <div className="row">
           <div className="col-lg-12 col-sm-6 d-flex justify-content-center align-items-center">
-            <span className="fs-5 mx-2"> Having technical issues? </span>
+            <span className="fs-5 mx-2">
+              {" "}
+              {language === "ENG"
+                ? "Having technical issues?"
+                : "तकनीकी समस्याएँ हैं?"}
+            </span>
             <a
               className="text-info fs-5"
               href="mailto:ankit@shuniyavigyan.com"
               subject="कोड.com Problems"
               style={{ textDecoration: "underline" }}
             >
-              Mail us your query!
+              {language === "ENG"
+                ? "Mail us your query!"
+                : "अपनी समस्या हमें मेल करें!"}
             </a>
           </div>
         </div>
